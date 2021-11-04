e8b7f1f0ec689b2fce27f1b31e9e7618afa3fd07
Ryan Mark <rymark907@gmail.com>
2021-11-04 16:54:18 -0700

    Formating data recieved by api

--- api/model.js
+++ api/model.js
@@ -4,7 +4,18 @@ async function getRecipeById() {
     const result = await db('recipes as r')
         .join('steps as s', 's.recipe_id', 'r.recipe_id')
         .join('ingredients as in')
-    return result
+        .join('steps_ingredients as si')
+        .select('r.recipe_id', 'r.recipe_name', 'r.created_at', 's.step_number', 's.step_instructions', 's.step_id', 'in.ingredient_name', 'in.ingredient_id', 'quantity')
+    // return result
+
+    const recipe = { steps:[] }
+
+    result.map(recipes => {
+        recipe.recipe_name = recipes.recipe_name
+        recipe.recipe_id = recipes.recipe_id
+        recipe.created_at = recipes.created_at
+    })
+    return recipe
 }
 
 module.exports = {
--- data/migrations/20211104191706_first-migration.js
+++ data/migrations/20211104191706_first-migration.js
@@ -21,16 +21,9 @@ exports.up = function(knex) {
         table.increments('ingredient_id')
         table.string('ingredient_name').notNullable().unique()
     })
-    .createTable('recipe_ingredients', table => {
-        table.increments('recipe_ingredients_id')
+    .createTable('steps_ingredients', table => {
+        table.increments('steps_ingredients_id')
         table.decimal('quantity')
-        table.integer('recipe_id')
-            .unsigned()
-            .notNullable()
-            .references('recipe_id')
-            .inTable('recipes')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT')
         table.integer('ingredient_id')
             .unsigned()
             .notNullable()
