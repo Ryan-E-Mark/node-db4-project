043da7b407ac65c9d5308ab64ea093e450da84e0
Ryan Mark <rymark907@gmail.com>
2021-11-04 17:25:52 -0700

    data is there, just have to javascript it into the right shape

--- api/model.js
+++ api/model.js
@@ -2,11 +2,11 @@ const db = require('../data/db-config')
 
 async function getRecipeById() {
     const result = await db('recipes as r')
-        .join('steps as s', 's.recipe_id', 'r.recipe_id')
-        .join('ingredients as in')
-        .join('steps_ingredients as si')
+        .innerJoin('steps as s', 's.recipe_id', 'r.recipe_id')
+        .innerJoin('ingredients as in')
+        .innerJoin('steps_ingredients as si', 'si.step_id', 's.step_id')
         .select('r.recipe_id', 'r.recipe_name', 'r.created_at', 's.step_number', 's.step_instructions', 's.step_id', 'in.ingredient_name', 'in.ingredient_id', 'quantity')
-    // return result
+    // return result    
 
     const recipe = { steps:[] }
 
@@ -14,6 +14,16 @@ async function getRecipeById() {
         recipe.recipe_name = recipes.recipe_name
         recipe.recipe_id = recipes.recipe_id
         recipe.created_at = recipes.created_at
+        recipe.steps.push({
+            step_number: recipes.step_number,
+            step_instructions: recipes.step_instructions,
+            step_id: recipes.step_id,
+            ingredients: [{
+                ingredient_name: recipes.ingredient_name,
+                ingredient_id: recipes.ingredient_id,
+                quantity: recipes.quantity
+            }],
+        })
     })
     return recipe
 }
